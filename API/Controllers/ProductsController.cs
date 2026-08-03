e468c63fd18665c313ea0744328c8551386d8f7a
Jirin <jirinjain2001@gmail.com>
2026-08-03 20:45:24 +0530

    Third Commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,9 +13,9 @@ public class ProductsController(IProductRepository repo) : ControllerBase
 {
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        return Ok(await repo.GetProductsAsync());
+        return Ok(await repo.GetProductsAsync(brand, type, sort));
     }
 
     [HttpGet("{id:int}")] // api/products/2
@@ -74,6 +74,18 @@ public class ProductsController(IProductRepository repo) : ControllerBase
         }
 
         return BadRequest("Problem Deleting Product");
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        return Ok(await repo.GetTypesAsync());
     }
 
     private bool ProductExists(int id)
