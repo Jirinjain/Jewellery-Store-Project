113fe6858fd8ff81a7f480cfa1f9b20ca9dcfdab
Jirin <jirinjain2001@gmail.com>
2026-08-19 22:21:16 +0530

    Eight Commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient);
-  protected readonly title = 'Jewellery Store';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.error(error),
-      complete: () => console.log('complete')
-    })
-  }
+export class AppComponent {
+  title = 'Jewellery Store';
 }
